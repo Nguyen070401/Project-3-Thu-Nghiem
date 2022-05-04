da8ae1c239a2f0e3084371d31d518e430f655d39
KNP <19520187@gm.uit.edu.vn>
2022-05-04 11:02:38 +0700

    first commit

---
+++ src/components/HomePage1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+import './HomePage1.css';
+
+function HomePage1() {
+  return (
+    <>
+        <div className="hero-container">
+            <img alt="Homepage Pic" className="img-homepage" src='/images/img-1.jpg'/>
+            <h1 className='products'>PRODUCTS</h1>
+        </div>
+    </>
+  )
+}
+
+export default HomePage1
---
+++ src/components/pages/Products.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePage1 from '../HomePage1';
+
+function Products() {
+  return (
+    <>
+    <HomePage1 />
+  </>
+  )
+}
+
+export default Products
